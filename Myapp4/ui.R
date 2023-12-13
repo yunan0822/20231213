b4b3ee324950165469e24e240a88b27c864887e2
le402n40\uer <ccannlin@gmail.com>
2023-12-13 16:55:32 +0800

    conditionalPanel

--- Myapp4/ui.R
+++ Myapp4/ui.R
@@ -1,21 +1,25 @@
 library(shiny)
-
 shinyUI(pageWithSidebar(
   
-  headerPanel("Sample size for Case Control study"),
+  headerPanel("Generating distribution"),
   
   sidebarPanel(
-    numericInput("alpha", "Two-sided significance level:", min = 0.001, max = 0.999, value = 0.05),
-    numericInput("power", "Power:", min = 0.001, max = 0.999, value = 0.8),
-    numericInput("OR", "Smallest difference of clinical/biological importance:", min = 0.01, max = 100, value = 1.5),
-    numericInput("r", "The ratio of Case/Control:", min = 0.01, max = 100, value = 1),
-    numericInput("p0", "Proportion of controls with exposures:", min = 0.001, max = 0.999, value = 0.2)    
+    selectInput("method", "Choose distribution:", choices=c("Normal"="norm", "Student t"="st")),
+    helpText("Setting parameter(s) for distribution model"),
+    conditionalPanel(condition="input.method=='norm'",
+                     numericInput(inputId="mu", label="mean", value=0),
+                     numericInput(inputId="sd", label="standard deviation", value=1, min=0)
+    ),
+    conditionalPanel(condition="input.method=='st'",
+                     numericInput(inputId="df", label="Df", value=10, min=1)
+    ),
+    sliderInput(inputId="obs", 
+                label="Number of observations:", 
+                min = 1, max = 1000, value = 500)
   ),
   
   mainPanel(
-    h3(textOutput("text1")),
-    h4(textOutput("text2")),
-    h3(textOutput("text3")),
-    h4(textOutput("text4"))
+    plotOutput("distPlot")
   )
+  
 ))
