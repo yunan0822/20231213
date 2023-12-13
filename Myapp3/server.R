8c3ec1d31cd22b71ca6debbb4357c18fe4f26b1d
le402n40\uer <ccannlin@gmail.com>
2023-12-13 16:43:17 +0800

    Sample size calculator

--- Myapp3/server.R
+++ Myapp3/server.R
@@ -1,18 +1,18 @@
 library(shiny)
 
-# Define server logic required to generate and plot a random distribution
+# Your function
+Samplesize = function(Sig,p,X) {
+  Z = qnorm(1-Sig/2)
+  n = p*(1-p)*Z^2/X^2
+  n
+}
+
+# Define server logic required to calculate the needed sample size
 shinyServer(function(input, output) {
   
-  # Expression that generates a plot of the distribution. The expression is
-  # wrapped in a call to renderPlot to indicate that:
-  # 
-  # 1) It is 'reactive' and therefore should be automatically re-executed
-  # when inputs change 2) Its output type is a plot
-  output$distPlot = renderPlot({
-    
-    # generate an rnorm distribution and plot it
-    dist = rnorm(input$obs,mean=input$M,sd=input$S)
-    hist(dist,col=input$Color)
+  output$Size = renderText({
+    N = Samplesize(Sig = input$Sig,p = input$p, X = input$X)      
+    paste("We need",round(N),"samples for this sampling.")
   })
   
 })
--- Myapp3/ui.R
+++ Myapp3/ui.R
@@ -1,21 +1,20 @@
 library(shiny)
 
-# Define UI for application that plots random distributions 
+# Define UI for application that calculate the needed sample size 
 shinyUI(pageWithSidebar(
   
   # Application title
-  headerPanel("Hello FJU!"),
+  headerPanel("Sample size calculator"),
   
-  # Sidebar with inputs for number of observations, mean, SD, and coler.
+  # Sidebar with numeric inputs for significance level, expected rate, acceptable error range.
   sidebarPanel(
-    sliderInput("obs", "Number of observations:", min = 0, max = 1000, value = 500),
-    numericInput("M", "Mean of this normal distribution:", min = -200, max = 200, value = 100),
-    numericInput("S", "SD of this normal distribution:", min = 0, max = 50, value = 10),
-    radioButtons("Color", "Select the color of histogram:", choices = c("Red" = "red", "Blue" = "blue", "Green" = "green"))
+    numericInput("Sig", "Two-sided significance level:", min = 0, max = 1.000, value = 0.05),
+    numericInput("p", "The expected rate:", min = 0, max = 1, value = 0.5),
+    numericInput("X", "Acceptable error range:", min = 0, max = 1, value = 0.03)
   ),
   
-  # Show a plot of the generated distribution
+  # Show the outcome
   mainPanel(
-    plotOutput("distPlot")
+    h3(textOutput("Size"))
   )
 ))
